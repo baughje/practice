b5cd4d5f9a434030d31697eb3662d55ef3b1bb49
Jeb <jeb.baugh+git@gmail.com>
2012-02-11 13:32:58 -0600

    looping

--- flashcard.rb
+++ flashcard.rb
@@ -1,13 +1,29 @@
+$i = 0;
+$num = 5;
+
+until $i == $num do
+  
 card1 = rand(10)
 card2 = rand(10)
 correct_answer = card1 + card2
 
 puts "What is #{card1} + #{card2}?"
-submitted_answer = gets
-i = submitted_answer.to_i
+submitted_answer = gets.chomp
+integer_answer = submitted_answer.to_i
 
-if correct_answer != i then
-  puts "Wrong"
+if correct_answer != integer_answer then
+  puts "Wrong\n\n"
 else
-  puts "Correct"
+  puts "Correct\n\n"
 end
+
+$i += 1;
+end
+=begin
+
+total_number
+number_correct
+
+# elapsed time 
+
+=end
