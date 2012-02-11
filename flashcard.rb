b283f17db5f9fadd3f32e0fadecb795ee929e43f
Jeb <jeb.baugh+git@gmail.com>
2012-02-11 14:54:46 -0600

    trying to extend functionality

--- flashcard.rb
+++ flashcard.rb
@@ -1,7 +1,11 @@
 $i = 0;
 $num = 5;
-
+puts Time.now
 until $i == $num do
+  
+$wrong = 0;
+$correct = 0;
+$total = 0;
   
 card1 = rand(10)
 card2 = rand(10)
@@ -10,15 +14,21 @@ correct_answer = card1 + card2
 puts "What is #{card1} + #{card2}?"
 submitted_answer = gets.chomp
 integer_answer = submitted_answer.to_i
+$total += 1;
 
 if correct_answer != integer_answer then
-  puts "Wrong\n\n"
+  puts "Wrong\n\n";
+  $wrong += 1;
 else
-  puts "Correct\n\n"
+  puts "Correct\n\n";
+  $correct += 1;
 end
 
 $i += 1;
+puts "You got #{$wrong} wrong out of #{$total}.\n\n"
+
 end
+
 =begin
 
 total_number
