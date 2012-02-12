32ff711eae8f927736b54ea85ebea88e7c85be56
Jeb <jeb.baugh+git@gmail.com>
2012-02-12 09:37:07 -0600

    My Codecademy projects

---
+++ starting_a_startup.js
@@ -0,0 +1,22 @@
+/*
+What are my total costs this year? conditionals project from Codecademy
+*/
+
+var calculateTotalCosts = function(salary, numWorkers, city) {
+  var fixedCosts = 5000;
+  var variableCosts = salary * numWorkers;
+  
+  if (city === "NYC") {
+    return fixedCosts + variableCosts + 30000;
+  }
+  else if (city === "BEJ") {
+    return fixedCosts + variableCosts + 25000;
+  }
+  else {
+    return fixedCosts + variableCosts + 10000;
+  }
+};
+  
+console.log(calculateTotalCosts(50000, 9, "NYC"));
+console.log(calculateTotalCosts(50000, 9, "BEJ"));
+console.log(calculateTotalCosts(50000, 9, "MUM"));
