c409794732af66d11cc4c3ef8039b75981d11cae
Jeb <jeb.baugh+git@gmail.com>
2012-02-11 20:57:06 -0600

    stuffs change

--- addition_flashcard.rb
+++ addition_flashcard.rb
@@ -1,11 +1,12 @@
 $total = 0;
 $num = 5;
 $wrong = 0;
+$correct = 0;
+#$start_time = Time.now;
+#$total_time = Time.now - $start_time;
 
 puts Time.now
 until $total == $num do
-  
-$correct = 0;
   
 card1 = rand(10)
 card2 = rand(10)
@@ -16,18 +17,27 @@ submitted_answer = gets.chomp
 integer_answer = submitted_answer.to_i
 
 if correct_answer != integer_answer then
-  puts "Wrong\n\n";
+  puts "Wrong\n";
   $wrong += 1;
 else
-  puts "Correct\n\n";
+  puts "Correct\n";
   $correct += 1;
 end
-
 $total += 1;
-
 end
 
-puts "You got #{$wrong} wrong out of #{$total}.\n\n"
+if $wrong > $correct then
+  puts "You got more wrong than right.  You need more practice!\n\n";
+  elsif $wrong == 0 then
+    puts "You are a rock star!\n"
+    puts "You got all #{$total} cards right!\n\n"
+elsif $wrong < $correct then
+  puts "You got #{$wrong} wrong out of #{$total}.\n\n"
+  puts "You got more right than wrong.  Nice job.\n\n"
+else
+#puts "You got #{$wrong} wrong out of #{$total}.\n\n"
+#puts "You answered #{$correct} correctly.  Good job!\n\n"
+end
 
 =begin
 
