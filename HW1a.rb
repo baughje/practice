c6897320c32ce048a1dc60fdc3bbf7c781b231fc
Jeb <jeb.baugh+git@gmail.com>
2012-02-27 09:00:12 -0600

    Update HW1a.rb

--- HW1a.rb
+++ HW1a.rb
@@ -1,8 +1,7 @@
 def palindrome?(string)
-  string.downcase!
-	string.gsub!(/[\,\.\?\!\:\;\'\s+\"]/,"")
-	if string.reverse == string
-	  puts "true"
+  string = string.downcase.gsub(/[\,\.\?\!\:\;\'\s+\"]/,"")
+  if string.reverse == string
+    puts "true"
   else
     puts "false"
   end
